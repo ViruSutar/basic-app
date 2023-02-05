b660d3c8ddd87b6b871b7a3456c5be7081c3e12f
ViruSutar <72197422+ViruSutar@users.noreply.github.com>
2023-02-05 15:11:57 +0530

    try catch block added

--- Controller/UsersController.js
+++ Controller/UsersController.js
@@ -2,66 +2,88 @@ const User = require("../Models/User");
 
 class UsersController {
   static async createUser(req, res) {
-    let { name } = req.body;
+    try {
+      let { name } = req.body;
 
-    const user = await User.create({ name });
+      const user = await User.create({ name });
 
-    return res
-      .status(200)
-      .send(`User created successfully with id ${user._id}`);
+      return res
+        .status(200)
+        .send(`User created successfully with id ${user._id}`);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   static async updateUser(req, res) {
-    let { userId, name } = req.body;
+    try {
+      let { userId, name } = req.body;
 
-    const user = await User.findOne({ _id: userId });
+      const user = await User.findOne({ _id: userId });
 
-    if (!user) {
-      return res.status(400).send("User with this id not found");
+      if (!user) {
+        return res.status(400).send("User with this id not found");
+      }
+
+      if (name) user.name = name;
+
+      user.save();
+
+      return res.status(200).send("User updated successfully");
+    } catch (error) {
+      console.log(error);
     }
-
-    if (name) user.name = name;
-
-    user.save();
-
-    return res.status(200).send("User updated successfully");
   }
 
   static async listUsers(req, res) {
-    const users = await User.find({}, { name: 1 });
+    try {
+      const users = await User.find({}, { name: 1 });
 
-    return res.status(200).send(users.length !== 0 ? users : "Users not found");
+      return res
+        .status(200)
+        .send(users.length !== 0 ? users : "Users not found");
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   static async getUserById(req, res) {
-    const { userId } = req.query;
+    try {
+      const { userId } = req.query;
 
-    if (!userId) {
-      return res.status(400).send("User id required");
+      if (!userId) {
+        return res.status(400).send("User id required");
+      }
+
+      const user = await User.findOne({ _id: userId }, { name: 1 });
+
+      return res.status(200).send(user ? user : "User not found");
+    } catch (error) {
+      console.log(error);
     }
-
-    const user = await User.findOne({_id:userId}, { name: 1 });
-
-    return res.status(200).send(user ? user : "User not found");
   }
 
   static async deleteUser(req, res) {
-    const { userId } = req.body;
+    try {
+      const { userId } = req.body;
 
-    if (!userId) {
-      return res.status(400).send("UserId required");
+      if (!userId) {
+        return res.status(400).send("UserId required");
+      }
+
+      const user = await User.findOne({ _id: userId });
+
+      if (!user) {
+        return res.status(400).send("User with this id not found");
+      }
+
+      user.delete();
+      user.save();
+
+      return res.status(200).send("User deleted successfully");
+    } catch (error) {
+      console.log(error);
     }
-
-    const user = await User.findOne({ _id: userId });
-
-    if (!user) {
-      return res.status(400).send("User with this id not found");
-    }
-
-    user.delete();
-    user.save();
-
-    return res.status(200).send("User deleted successfully");
   }
 }
 
